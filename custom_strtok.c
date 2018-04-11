03f7d01bdc4c3c816302cf8cceeeab0288671d0e
Mayur Shingote <mayur.shingote@yahoo.com>
2018-04-12 01:53:12 +0545

    I have tried to implement c like strtok function.
    Updated variable names and positions for better readability.

--- custom_strtok.c
+++ custom_strtok.c
@@ -4,13 +4,13 @@
 
 #pragma region Standard strtok
 
-void standard_strtok(char *s, const char *sep)
+void standard_strtok(char *string, const char *separators)
 {
-	char *tok = strtok(s, sep);
-	while (tok)
+	char *token = strtok(string, separators);
+	while (token)
 	{
-		printf("%s\n", tok);
-		tok = strtok(NULL, sep);
+		printf("%s\n", token);
+		token = strtok(NULL, separators);
 	}
 }
 
@@ -22,6 +22,10 @@ char *global_pointer;
 char* my_strtok(char *string, const char *separators)
 {
 	static int index = 0;
+	int string_at = 0;
+	int separators_at = 0;
+	int string_length = 0;
+	int token_length = 0;
 
 	if (!string && index)
 	{
@@ -35,10 +39,9 @@ char* my_strtok(char *string, const char *separators)
 	{
 		return NULL;
 	}
-	int string_length = strlen(&string[index]);
-	int token_length = strlen(separators);
-	int string_at = 0;
-	int separators_at = 0;
+
+	string_length = strlen(&string[index]);
+	token_length = strlen(separators);
 	for (string_at = 0; string_at < string_length; ++string_at)
 	{
 		for (separators_at = 0; separators_at < token_length; ++separators_at)
@@ -55,13 +58,13 @@ char* my_strtok(char *string, const char *separators)
 	return string;
 }
 
-void custom_strtok(char s[], const char *sep)
+void custom_strtok(char string[], const char *separators)
 {
-	char *tok = my_strtok(s, sep);
-	while (tok)
+	char *token = my_strtok(string, separators);
+	while (token)
 	{
-		printf("%s\n", tok);
-		tok = my_strtok(NULL, sep);
+		printf("%s\n", token);
+		token = my_strtok(NULL, separators);
 	}
 }
 
@@ -69,12 +72,12 @@ void custom_strtok(char s[], const char *sep)
 
 int main()
 {
-	char s1[] = "I#am,a*programmer";
-	char s2[] = "I#am,a*programmer";
+	char for_standard_strtok[] = "I#am,a*programmer";
+	char for_custom_strtok[] = "I#am,a*programmer";
 	const char *separators = "#,*";
 
-	standard_strtok(s1, separators);
-	custom_strtok(s2, separators);
+	standard_strtok(for_standard_strtok, separators);
+	custom_strtok(for_custom_strtok, separators);
 
 	getchar();
 	return 0;
